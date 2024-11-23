cb8579d952facbc7a8ca306a5964faa8d037ebd9
Felipe Almeida de Carvalho <flpalmeidac@gmail.com>
2024-11-22 23:20:14 -0300

    up 3d visualization

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-import PIL
 import cv2
 import os
 import numpy as np
+import skimage
+import matplotlib.pyplot as plt
+import pyvista as pv
 
 dir = os.path.dirname(os.path.abspath(__file__))
 dirPasta = 'imgsPasta'
 dirPastaCrop = 'imgsCrop'
 dirSinogramas = 'sinogramas'
+dirPastaRadon = 'radonPasta'
+dirPastaRadon2 = 'radonPasta2'
 
 os.makedirs(dirPastaCrop, exist_ok=True)
+os.makedirs(dirSinogramas, exist_ok=True)
+os.makedirs(dirPastaRadon, exist_ok=True)
 
 imgTeste = cv2.imread('imgsPasta/teste.jpg')
 alt, larg = imgTeste.shape[:2]
@@ -53,7 +59,46 @@ for i in range(0, len(imgsFinal), numGrupo):
 
     if imgsCombinadas:
         sinograma = np.hstack(imgsCombinadas)
+        sinogramaCinza = cv2.cvtColor(sinograma, cv2.COLOR_BGR2GRAY)
+        sinogramaNormalizado = cv2.normalize(sinogramaCinza, None, 0, 255, cv2.NORM_MINMAX)
 
         contGrupo += 1
         dirImgCombinada = os.path.join(dir, f'{dirSinogramas}/sinograma{contGrupo}.jpg')
-        cv2.imwrite(dirImgCombinada, sinograma)
+        cv2.imwrite(dirImgCombinada, (sinogramaNormalizado * 255).astype(np.uint8))
+
+        theta = np.linspace(0., 180., max(sinogramaNormalizado.shape), endpoint=False)
+        imgRadon = skimage.transform.radon(sinogramaNormalizado, theta=theta, circle=False)
+
+        radonNorm = cv2.normalize(imgRadon, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+        dirRadon = os.path.join(dir, f'{dirPastaRadon}/radon{contGrupo}.jpg')
+        cv2.imwrite(dirRadon, radonNorm)
+
+imgsRadon = []
+for arq in os.listdir(dirPastaRadon):
+    if arq.lower().endswith('.jpg'):
+        dirImg = os.path.join(dirPastaRadon, arq)
+        img = cv2.imread(dirImg, cv2.IMREAD_GRAYSCALE)
+        if img is not None:
+            imgsRadon.append(img)
+
+vol = np.stack(imgsRadon, axis=0)
+
+camReconstruidas = []
+for imgRadon in imgsRadon:
+    theta = np.linspace(0., 180., imgRadon.shape[1], endpoint=False)
+    camReconstruida = skimage.transform.iradon(imgRadon, theta=theta, circle=False)
+    camReconstruidas.append(camReconstruida)
+    dirRadon2 = os.path.join(dir, f'{dirPastaRadon2}/radon{contGrupo}.jpg')
+    cv2.imwrite(dirRadon2, camReconstruida)
+
+volume = np.stack(camReconstruidas, axis=0)
+volumePv = pv.wrap(volume)
+plotter = pv.Plotter()
+plotter.add_volume(volumePv, opacity="sigmoid_3")
+plotter.show()
+
+fig, axes = plt.subplots(1, 3, figsize=(12, 4))
+axes[0].imshow(volume[volume.shape[0]//2], cmap="gray")
+axes[1].imshow(volume[volume.shape[0]//4], cmap="gray")
+axes[2].imshow(volume[-1], cmap="gray")
+plt.show()
