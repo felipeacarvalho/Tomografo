c22c10e5d3094565866f3c6e59201caa840ff941
Felipe Almeida de Carvalho <flpalmeidac@gmail.com>
2024-11-23 21:21:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import skimage
 import matplotlib.pyplot as plt
 import pyvista as pv
-
+    
 dir = os.path.dirname(os.path.abspath(__file__))
 dirPasta = 'imgsPasta'
 dirPastaCrop = 'imgsCrop'
@@ -12,66 +12,69 @@ dirSinogramas = 'sinogramas'
 dirPastaRadon = 'radonPasta'
 dirPastaRadon2 = 'radonPasta2'
 
+numGrupo = 99
+contGrupo = 0
+
 os.makedirs(dirPastaCrop, exist_ok=True)
 os.makedirs(dirSinogramas, exist_ok=True)
 os.makedirs(dirPastaRadon, exist_ok=True)
 
-imgTeste = cv2.imread('imgsPasta/teste.jpg')
-alt, larg = imgTeste.shape[:2]
-centro = (larg // 2, alt // 2)
-angulo = 88.9
+if input("Deseja tratar as imagens novamente? (s/n)") == ('s' or 'ss' or 'sim' or 'y' or 'yes'):
 
-matrizRotacao = cv2.getRotationMatrix2D(centro, angulo, scale=1)
+    print('Processando...')
 
+    imgTeste = cv2.imread('imgsPasta/teste.jpg')
+    alt, larg = imgTeste.shape[:2]
+    centro = (larg // 2, alt // 2)
+    angulo = 88.9
 
-for foto in os.listdir(dirPasta):
-    dirFoto = os.path.join(dirPasta, foto)
+    matrizRotacao = cv2.getRotationMatrix2D(centro, angulo, scale=1)
 
-    if foto.lower().endswith('.jpg'):
-        img = cv2.imread(dirFoto)
+    for foto in os.listdir(dirPasta):
+        dirFoto = os.path.join(dirPasta, foto)
 
-        if img is not None:
-            imgRot = cv2.warpAffine(img, matrizRotacao, (larg, alt))
+        if foto.lower().endswith('.jpg'):
+            img = cv2.imread(dirFoto)
 
-            imgCrop = imgRot[0:400, 195:199]
+            if img is not None:
+                imgRot = cv2.warpAffine(img, matrizRotacao, (larg, alt))
 
-            cv2.imwrite(f'{dirPastaCrop}/crop{foto}', imgCrop)
+                imgCrop = imgRot[0:400, 195:199]
 
-numGrupo = 99
-contGrupo = 0
+                cv2.imwrite(f'{dirPastaCrop}/crop{foto}', imgCrop)
 
-imgsFinal = [img  for img in os.listdir(dirPastaCrop) if img.lower().endswith('.jpg')]
+    imgsFinal = [img  for img in os.listdir(dirPastaCrop) if img.lower().endswith('.jpg')]
 
-if imgsFinal:
-    imgsFinal = imgsFinal[:-1]
+    if imgsFinal:
+        imgsFinal = imgsFinal[:-1]
 
-for i in range(0, len(imgsFinal), numGrupo):
-    imgsCombinadas = []
+    for i in range(0, len(imgsFinal), numGrupo):
+        imgsCombinadas = []
 
-    grupoTratado = imgsFinal[i:i+numGrupo]
+        grupoTratado = imgsFinal[i:i+numGrupo]
 
-    for imgTratada in grupoTratado:
-        dirCrop = os.path.join(dirPastaCrop, imgTratada)
-        img = cv2.imread(dirCrop)
+        for imgTratada in grupoTratado:
+            dirCrop = os.path.join(dirPastaCrop, imgTratada)
+            img = cv2.imread(dirCrop)
 
-        if img is not None:
-            imgsCombinadas.append(img)
+            if img is not None:
+                imgsCombinadas.append(img)
 
-    if imgsCombinadas:
-        sinograma = np.hstack(imgsCombinadas)
-        sinogramaCinza = cv2.cvtColor(sinograma, cv2.COLOR_BGR2GRAY)
-        sinogramaNormalizado = cv2.normalize(sinogramaCinza, None, 0, 255, cv2.NORM_MINMAX)
+        if imgsCombinadas:
+            sinograma = np.hstack(imgsCombinadas)
+            sinogramaCinza = cv2.cvtColor(sinograma, cv2.COLOR_BGR2GRAY)
+            sinogramaNormalizado = cv2.normalize(sinogramaCinza, None, 0, 255, cv2.NORM_MINMAX)
 
-        contGrupo += 1
-        dirImgCombinada = os.path.join(dir, f'{dirSinogramas}/sinograma{contGrupo}.jpg')
-        cv2.imwrite(dirImgCombinada, (sinogramaNormalizado * 255).astype(np.uint8))
+            contGrupo += 1
+            dirImgCombinada = os.path.join(dir, f'{dirSinogramas}/sinograma{contGrupo}.jpg')
+            cv2.imwrite(dirImgCombinada, (sinogramaNormalizado * 255).astype(np.uint8))
 
-        theta = np.linspace(0., 180., max(sinogramaNormalizado.shape), endpoint=False)
-        imgRadon = skimage.transform.radon(sinogramaNormalizado, theta=theta, circle=False)
+            theta = np.linspace(0., 180., max(sinogramaNormalizado.shape), endpoint=False)
+            imgRadon = skimage.transform.radon(sinogramaNormalizado, theta=theta, circle=False)
 
-        radonNorm = cv2.normalize(imgRadon, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
-        dirRadon = os.path.join(dir, f'{dirPastaRadon}/radon{contGrupo}.jpg')
-        cv2.imwrite(dirRadon, radonNorm)
+            radonNorm = cv2.normalize(imgRadon, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+            dirRadon = os.path.join(dir, f'{dirPastaRadon}/radon{contGrupo}.jpg')
+            cv2.imwrite(dirRadon, radonNorm)
 
 imgsRadon = []
 for arq in os.listdir(dirPastaRadon):
@@ -96,9 +99,3 @@ volumePv = pv.wrap(volume)
 plotter = pv.Plotter()
 plotter.add_volume(volumePv, opacity="sigmoid_3")
 plotter.show()
-
-fig, axes = plt.subplots(1, 3, figsize=(12, 4))
-axes[0].imshow(volume[volume.shape[0]//2], cmap="gray")
-axes[1].imshow(volume[volume.shape[0]//4], cmap="gray")
-axes[2].imshow(volume[-1], cmap="gray")
-plt.show()
